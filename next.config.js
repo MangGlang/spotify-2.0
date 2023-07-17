d8b12ddb02e1f1250a81941255c5b3666e380b0d
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-07-17 06:41:25 -0700

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "iqpqmnrvzjbbwnjalunn.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
